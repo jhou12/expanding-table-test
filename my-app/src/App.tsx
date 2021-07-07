4df52534581b0635ea0a3a5a52bd391b8f66183e
Jenny Hou <jhou2012@gmail.com>
2021-07-07 16:09:28 -0400

    refactor to one onClick

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,8 +6,8 @@ import { TablePlain } from './Table';
 
 function App() {
   const mainRef: any = useRef()
-  const colRef: any = useRef()
-  const colRef0: any = useRef()
+  const colRef2: any = useRef()
+  const colRef1: any = useRef()
   const contRef: any = useRef()
   const contRef0: any = useRef()
   const contRef3: any = useRef()
@@ -18,58 +18,10 @@ function App() {
   const ref4: any = useRef()
   const loc: any = useRef()
 
-  const onClick = (e: any) => {
-    console.log('ORIG ONCLICK')
-    const cont: any = contRef.current
-    const col: any = colRef.current
-    const t = ref.current
-
-    const x = col?.offsetLeft
-
-    const y = cont?.offsetTop
-    const w = cont?.offsetWidth
-    const h = cont?.offsetHeight
-    cont.style.setProperty('--loc-x', x + 'px')
-    cont.style.setProperty('--loc-y', y + 'px')
-    cont.style.setProperty('--loc-w', w + 'px')
-    cont.style.setProperty('--loc-h', h + 'px')
-
-    if (ref.current?.classList.contains('table--abs')) {
-
-      t.style.setProperty('--loc-x', x + 8 + 'px')
-      t.style.removeProperty('--loc-y')
-      t.style.removeProperty('--loc-w')
-      t.style.removeProperty('--loc-h')
-      setTimeout(() => {
-        ref.current.classList.remove('table--abs')
-      }, 1000)
-    } else {
-      cont.classList.add('container--filler')
-
-      loc.current = {
-        x: t.offsetLeft,
-        y: t.offsetTop,
-        w: t.offsetWidth,
-        h: t.offsetHeight
-      }
-      t.style.setProperty('--loc-x', t.offsetLeft + 'px')
-      t.style.setProperty('--loc-y', t.offsetTop + 'px')
-      t.style.setProperty('--loc-w', t.offsetWidth + 'px')
-      t.style.setProperty('--loc-h', t.offsetHeight + 'px')
-      t.classList.add('table--abs')
-      setTimeout(() => {
-        t.style.setProperty('--timing', '1s')
-        t.style.setProperty('--loc-x', 0 + 'px')
-        t.style.setProperty('--loc-y', 0 + 'px')
-        t.style.setProperty('--loc-w', mainRef.current?.offsetWidth + 'px')
-        t.style.setProperty('--loc-h', mainRef.current.offsetHeight + 'px')
-      }, 0)
-    }
-  }
-  const onClick2 = (e: any) => {
-    const cont: any = contRef0.current
-    const col: any = colRef0.current
-    const t = ref2.current
+  const onClick = (tableRef: any, containerRef: any, columnRef: any) => {
+    const cont: any = containerRef.current
+    const col: any = columnRef.current
+    const t = tableRef.current
 
     const x = col?.offsetLeft
     const y = cont?.offsetTop
@@ -80,104 +32,14 @@ function App() {
     cont.style.setProperty('--loc-w', w + 'px')
     cont.style.setProperty('--loc-h', h + 'px')
 
-    if (ref2.current?.classList.contains('table--abs')) {
+    if (tableRef.current?.classList.contains('table--abs')) {
+
       t.style.setProperty('--loc-x', x + 8 + 'px')
       t.style.removeProperty('--loc-y')
       t.style.removeProperty('--loc-w')
       t.style.removeProperty('--loc-h')
       setTimeout(() => {
-        ref2.current.classList.remove('table--abs')
-      }, 1000)
-    } else {
-      cont.classList.add('container--filler')
-
-      loc.current = {
-        x: t.offsetLeft,
-        y: t.offsetTop,
-        w: t.offsetWidth,
-        h: t.offsetHeight
-      }
-      t.style.setProperty('--loc-x', t.offsetLeft + 'px')
-      t.style.setProperty('--loc-y', t.offsetTop + 'px')
-      t.style.setProperty('--loc-w', t.offsetWidth + 'px')
-      t.style.setProperty('--loc-h', t.offsetHeight + 'px')
-      t.classList.add('table--abs')
-      setTimeout(() => {
-        t.style.setProperty('--timing', '1s')
-        t.style.setProperty('--loc-x', 0 + 'px')
-        t.style.setProperty('--loc-y', 0 + 'px')
-        t.style.setProperty('--loc-w', mainRef.current?.offsetWidth + 'px')
-        t.style.setProperty('--loc-h', mainRef.current.offsetHeight + 'px')
-      }, 0)
-    }
-  }
-  const onClick4 = (e: any) => {
-    const cont: any = contRef4.current
-    const col: any = colRef0.current
-    const t = ref4.current
-
-    const x = col?.offsetLeft
-    const y = cont?.offsetTop
-    const w = cont?.offsetWidth
-    const h = cont?.offsetHeight
-    cont.style.setProperty('--loc-x', x + 'px')
-    cont.style.setProperty('--loc-y', y + 'px')
-    cont.style.setProperty('--loc-w', w + 'px')
-    cont.style.setProperty('--loc-h', h + 'px')
-
-    if (ref4.current?.classList.contains('table--abs')) {
-      t.style.setProperty('--loc-x', x + 8 + 'px')
-      t.style.removeProperty('--loc-y')
-      t.style.removeProperty('--loc-w')
-      t.style.removeProperty('--loc-h')
-      setTimeout(() => {
-        ref4.current.classList.remove('table--abs')
-      }, 1000)
-    } else {
-      cont.classList.add('container--filler')
-
-      loc.current = {
-        x: t.offsetLeft,
-        y: t.offsetTop,
-        w: t.offsetWidth,
-        h: t.offsetHeight
-      }
-      t.style.setProperty('--loc-x', t.offsetLeft + 'px')
-      t.style.setProperty('--loc-y', t.offsetTop + 'px')
-      t.style.setProperty('--loc-w', t.offsetWidth + 'px')
-      t.style.setProperty('--loc-h', t.offsetHeight + 'px')
-      t.classList.add('table--abs')
-      setTimeout(() => {
-        t.style.setProperty('--timing', '1s')
-        t.style.setProperty('--loc-x', 0 + 'px')
-        t.style.setProperty('--loc-y', 0 + 'px')
-        t.style.setProperty('--loc-w', mainRef.current?.offsetWidth + 'px')
-        t.style.setProperty('--loc-h', mainRef.current.offsetHeight + 'px')
-      }, 0)
-    }
-  }
-  const onClick3 = (e: any) => {
-    const cont: any = contRef3.current
-    const col: any = colRef.current
-    const t = ref3.current
-
-    const x = col?.offsetLeft
-
-    const y = cont?.offsetTop
-    const w = cont?.offsetWidth
-    const h = cont?.offsetHeight
-    cont.style.setProperty('--loc-x', x + 'px')
-    cont.style.setProperty('--loc-y', y + 'px')
-    cont.style.setProperty('--loc-w', w + 'px')
-    cont.style.setProperty('--loc-h', h + 'px')
-
-    if (ref3.current?.classList.contains('table--abs')) {
-      t.style.setProperty('--loc-x', x + 8 + 'px')
-      t.style.removeProperty('--loc-y')
-      t.style.removeProperty('--loc-w')
-      t.style.removeProperty('--loc-h')
-      setTimeout(() => {
-        ref3.current.classList.remove('table--abs')
+        tableRef.current.classList.remove('table--abs')
       }, 1000)
     } else {
       cont.classList.add('container--filler')
@@ -204,13 +66,13 @@ function App() {
   }
   return (
     <div className="App" ref={mainRef}>
-      <div className="col" ref={colRef0}>
-        <div className="container" ref={contRef4}><div className="table sq" ref={ref4} onClick={onClick4}><TablePlain /></div></div>
-        <div className="container" ref={contRef0}><div className="table rect" ref={ref2} onClick={onClick2}><TablePlain /></div></div>
+      <div className="col" ref={colRef1}>
+        <div className="container" ref={contRef4}><div className="table sq" ref={ref4} onClick={() => onClick(ref4, contRef4, colRef1)}><TablePlain /></div></div>
+        <div className="container" ref={contRef0}><div className="table rect" ref={ref2} onClick={() => onClick(ref2, contRef0, colRef1)}><TablePlain /></div></div>
       </div>
-      <div className="col" ref={colRef}>
-        <div className="container" ref={contRef3}><div className="table rect" ref={ref3} onClick={onClick3}><TablePlain /></div></div>
-        <div className="container" ref={contRef}><div className="table sq" ref={ref} onClick={onClick}><TablePlain /></div></div>
+      <div className="col" ref={colRef2}>
+        <div className="container" ref={contRef3}><div className="table rect" ref={ref3} onClick={() => onClick(ref3, contRef3, colRef2)}><TablePlain /></div></div>
+        <div className="container" ref={contRef}><div className="table sq" ref={ref} onClick={() => onClick(ref, contRef, colRef2)}><TablePlain /></div></div>
       </div>
     </div>
   );
